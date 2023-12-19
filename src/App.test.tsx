fe3a46670d3d6fa90c6b490014ca96e2fb1dd39e
frank <frank@js.ie>
2023-12-19 10:01:25 +0000

    Cypress starting

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App.tsx';
+import App from './App';
 import {test, expect} from '@jest/globals';
 
 test('renders learn react link', () => {
